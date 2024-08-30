12a85b5ae4612d07a5f35dea46e71e97065c7232
C0mplexity <106925362+C0mplexity0@users.noreply.github.com>
2024-08-30 20:46:39 +0100

    Use single quotes for consistency

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,7 +1,7 @@
-import App from "./App";
-import { createRoot } from "react-dom/client";
+import App from './App';
+import { createRoot } from 'react-dom/client';
 
-const container = document.getElementById("root") as HTMLDivElement;
+const container = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(container);
 
 root.render(<App />);
