64f48ad44c7688beb5352105e59fa4152cfc0bcb
C0mplexity <106925362+C0mplexity0@users.noreply.github.com>
2024-08-30 20:44:00 +0100

    Add React code

---
+++ src/renderer/App.tsx
@@ -0,0 +1,10 @@
+import "./index.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>💖 Hello World!</h1>
+      <p>Welcome to your Electron application.</p>
+    </div>
+  );
+}
---
+++ src/renderer/index.tsx
@@ -0,0 +1,7 @@
+import App from "./App";
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("root") as HTMLDivElement;
+const root = createRoot(container);
+
+root.render(<App />);
